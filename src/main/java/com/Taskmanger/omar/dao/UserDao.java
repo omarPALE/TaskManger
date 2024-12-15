2bf98f3aeba56a7a1544deb280b9b943b42ec74d
omar <omar.alratrot@yahoo.com>
2024-12-16 01:49:36 +0200

    add jwt dependency and start working on auth  user

--- src/main/java/com/Taskmanger/omar/dao/UserDao.java
+++ src/main/java/com/Taskmanger/omar/dao/UserDao.java
@@ -4,7 +4,10 @@ import com.Taskmanger.omar.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 
 public interface UserDao extends JpaRepository<User, Integer> {
+    Optional<User> findByUsername(String username);
 }
